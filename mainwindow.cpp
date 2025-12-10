601aca0bdabe3d7359b97714f20a3da04dcf76ba
Curtain-Wang <wangqt1997@qq.com>
2025-12-10 16:07:02 +0800

    模式切换bug修复

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -921,6 +921,11 @@ void MainWindow::staticCanDataInit()
     lowByteIndexHash.insert(3, 4);
     highByteIndexHash.insert(3, -1);
     writeCanidHash.insert(3, 0x18E13A01);
+    //模式
+    canidHash.insert(2, 0x18E33501);
+    lowByteIndexHash.insert(2, 2);
+    highByteIndexHash.insert(2, -1);
+    writeCanidHash.insert(2, 0x18E13A01);
     //A侧输出电压
     canidHash.insert(0x0c, 0x18E63501);
     lowByteIndexHash.insert(0x0c, 0);
@@ -1368,6 +1373,6 @@ void MainWindow::on_pushButton_10_clicked()
 void MainWindow::on_cbx_workingmode_currentIndexChanged(int index)
 {
     quint16 value = index * 2 + 1 + (timingDataBuf[2] & 0xE0);
-    manualWriteOneCMDBuild(static_cast<char>(0), 2, value >> 8, value & 0xFF);
+    writeCanData(2, value >> 8, value & 0xFF);
 }
 
